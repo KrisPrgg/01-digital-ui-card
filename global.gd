a87fd2b1806662cc488662c503cc7244cd12e60c
Your Name <you@example.com>
2026-09-10 16:05:22 +0300

    esut

---
+++ global.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Обьявялем переменные 
+var hp 
+var hov 
+var bag 
+var wg 
+var name_user 
+
+# Задаем значения переменным 
+func set_info(_hp, _hov, _bag, _wg, _name): 
+	hp = _hp 
+	hov = _hov 
+	bag = _bag 
+	wg = _wg 
+	name_user = _name
